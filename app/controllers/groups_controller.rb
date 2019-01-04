bcc698e0b40e1b78a963aa9b89bcd0fe2bbf824b
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-04 17:40:44 +0900

    New group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,17 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
+    @group.users << current_user
   end
 
   def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
@@ -13,5 +21,7 @@ class GroupsController < ApplicationController
   end
 
   private
-
+  def group_params
+    params.require(:group).permit(:name, {:user_ids => []})
+  end
 end
