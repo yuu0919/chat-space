dc8fa5335dad4832cf488297232994580cd1197a
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-05 13:52:52 +0900

    Comp message

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :users, through: :members
   has_many :messages
   validates :name, presence: true
+
+  def show_last_message
+    if (last_message = messages.last).present?
+      last_message.body? ? last_message.body : '画像が投稿されています'
+    else
+      'まだメッセージがありません'
+    end
+  end
 end
