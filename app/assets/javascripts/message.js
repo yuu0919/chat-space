21d46c415a00485b3cba7121c215b58f1684b5ef
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-08 19:42:11 +0900

    Add jQuery

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,57 @@
+$(function(){
+  function buildHTML(message){
+    var body = `<div class="chat-main__message-name">
+                 ${message.user_name}
+               </div>
+               <div class="chat-main__message-time">
+                 ${message.created_at}
+               </div>
+               <div class="chat-main__message-body">
+                 <p class="lower-message__content">
+                   ${message.body}
+                 </p>`
+    if (message.image) {
+
+      var html = `<div class="chat-main__body--message-list">
+                    <div class="chat-main__message clearfix">
+                      ${body}
+                        <img class="lower-message__image" src=${message.image}>
+                    </div>
+                  </div>`
+    } else {
+      var html = `<div class="chat-main__body--message-list">
+                    <div class="chat-main__message clearfix">
+                      ${body}
+                    </div>
+                  </div>`
+    }
+    return html;
+  }
+
+  $("#form").on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData($(this).get()[0]);
+    console.log(formData);
+    var href = window.location.href;
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.chat-main__body').append(html);
+      $('#form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    })
+    .always(function(){
+      $('#form__submit').removeAttr('disabled');
+    });
+    $('.chat-main__body').animate({scrollTop: $('.chat-main__body')[0].scrollHeight}, 'fast');
+  })
+});
---
+++ app/views/messages/create.json.jbuilder
@@ -0,0 +1,5 @@
+json.id @message.id
+json.body  @message.body
+json.image  @message.image.url
+json.user_name  @message.user.name
+json.created_at  @message.created_at
