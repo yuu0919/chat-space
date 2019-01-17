4be28435a8fbc32c23f99c0780ca0445f4eabe7d
yuu0919 <yw3055.jenesice@gmail.com>
2019-01-17 20:40:13 +0900

    Change

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-listen 3000
+
 timeout 60
 
 preload_app true
